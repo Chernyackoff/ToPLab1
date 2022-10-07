1b829d1a1e31e3373e9807d5fb66b7e8169111b7
Chernyackoff <chernyackoff.vlad@yandex.ru>
2022-10-07 11:42:23 +0300

    Initialised project

--- Person.cpp
+++ Person.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Matebook on 07.10.2022.
-//
-
 #include "Person.hpp"
--- Person.hpp
+++ Person.hpp
@@ -1,4 +1,12 @@
 #ifndef LAB1_PERSON_HPP
 #define LAB1_PERSON_HPP
+#include <string>
 
+class Person{
+private:
+    string name;
+
+
+public:
+};
 #endif //LAB1_PERSON_HPP
